ee41112805f911b6243d13ca2c2a25eb410df03f
salamander <farhimzakirova@gmail.com>
2022-02-17 12:10:20 +0600

    change of directory

--- configme.py
+++ configme.py
@@ -1,9 +1,8 @@
-my_name = 'LMN'
-# cookie = {'__test': '145b9b50386188159e6294167e37c951'}  #roy
-cookie = {'__test': '299447a688f0c4c833fc1ad410d426ef'}  #s-bond
+my_name = ''
+cookie = {'__test': ''}  #s-bond
 crypto_key = 'vP1nWwtTo6in4-CqvOM_J3l0sXSebiJRk2n_tV1luCk='
-receive_time = 2
+receive_time = 2 # in second
 my_timezone = 'Asia/Dhaka'
 date_time_format = '%d-%b-%a %H:%M'
 #https://www.w3schools.com/python/python_datetime.asp
-base_url = 'http://willnervegearbeeofficial411333206873787.rf.gd/json-test'
+base_url = 'http://willnervegearbeeofficial411333206873787.rf.gd/messen-v1.2'
